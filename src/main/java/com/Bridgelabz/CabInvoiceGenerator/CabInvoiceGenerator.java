2b1f998d58d70fefc7318e1dbfb65782b9da63f0
manogupt <manojgupta123929@gmail.com>
2022-07-23 18:54:40 +0530

    Added : Tested Multiple Rides

--- src/main/java/com/Bridgelabz/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/com/Bridgelabz/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -1,17 +1,22 @@
 package com.Bridgelabz.CabInvoiceGenerator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CabInvoiceGenerator {
 
-	private final int costPerKm = 10;
-	private final int costPerMinute = 1;
-	public int minFare = 5;
-	double totalFare;
-
-	public double CalculateFare(double DISTANCE, int TIME) {
-
-		totalFare = costPerKm * DISTANCE + costPerMinute * TIME;
-		if (totalFare < minFare)
-			totalFare = minFare;
-		return totalFare;
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double TotalFare = cabInvoiceGenerator.CalculateFare(5.0, 4);
+		Assert.assertEquals(54, TotalFare, TotalFare);
 	}
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnMinFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double TotalFare = cabInvoiceGenerator.CalculateFare(0.1, 2);
+		Assert.assertEquals(5, TotalFare, TotalFare);
+	}
+
 }
